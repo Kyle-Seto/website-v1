1fb55925e97d15e7b7bff1387d966c2a2a641a8c
Kyle <k2seto@edu.uwaterloo.ca>
2020-03-08 14:17:05 -0400

    build

---
+++ build/precache-manifest.0e78ffc3af7259b9a61e6dc16fbd67fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef268ede73c1d3a44911f324ff3568a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74ef8f1a18f499006f4b",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d57c8fc69be04f5eefef",
+    "url": "/static/css/main.fba01ab6.chunk.css"
+  },
+  {
+    "revision": "74ef8f1a18f499006f4b",
+    "url": "/static/js/2.420a83fd.chunk.js"
+  },
+  {
+    "revision": "d57c8fc69be04f5eefef",
+    "url": "/static/js/main.d0430b9d.chunk.js"
+  },
+  {
+    "revision": "d715c210ce45128b27c6",
+    "url": "/static/js/runtime-main.8231d089.js"
+  },
+  {
+    "revision": "d362eab3659dc92652c3814bea442285",
+    "url": "/static/media/django.d362eab3.jpg"
+  },
+  {
+    "revision": "bea696ceedb859a9a8b6ed860e312407",
+    "url": "/static/media/racket.bea696ce.jpg"
+  },
+  {
+    "revision": "07105d67c4750acebd7d4f86051f43e0",
+    "url": "/static/media/website.07105d67.jpg"
+  }
+]);
